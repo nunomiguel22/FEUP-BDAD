a4153a100f6277ff5430fa94dd25aab698f5560c
TMBMarques <48417326+TMBMarques@users.noreply.github.com>
2020-05-21 05:18:18 +0100

    int2

---
+++ Terceira Entrega/int2.sql
@@ -0,0 +1,12 @@
+-- int2 _ obter salas com conteúdo adulto
+
+.mode columns
+.headers on
+.nullvalue NULL
+
+SELECT sala.nome, perfil.nome AS administrador, sala.avatar
+FROM sala
+INNER JOIN perfil
+ON sala.idperfil = perfil.idperfil
+WHERE adulto = 1
+GROUP BY sala.nome
